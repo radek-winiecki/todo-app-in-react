79cba4efaf657dba7a5c040501b61f91db73055e
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-09-06 15:10:27 +0200

    Create a sort function for the active tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -20,6 +20,22 @@ const TaskList = (props) => {
         })
     }
 
+    if (active.length >= 2) {
+        active.sort((a, b) => {
+
+            a = a.text.toLowerCase();
+            b = b.text.toLowerCase();
+
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            } else {
+                return  0;
+            }
+        })
+    }
+
     const activeTasks = active.map(task =>
         <Task
             key={task.id}
