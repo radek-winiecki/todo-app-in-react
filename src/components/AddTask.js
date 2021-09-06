eec78e6876da271da38abd71a3f8169a1f2d4af8
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-09-06 11:49:36 +0200

    Create a form for adding tasks

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -3,13 +3,18 @@ import {Component} from "react";
 
 class AddTask extends Component {
     state = {
-
+        text: '',
+        checked: false,
     }
     render() {
         return(
             <div className="form">
-                <div>Dodaj task</div>
-                <hr/>
+                <input type="text" placeholder="dodaj zadanie" value={this.state.text} />
+                <input type="checkbox" checked={this.state.checked} id="important"/>
+                <label htmlFor="important">Priorytet</label>
+                <label htmlFor="date">Do kiedy zrobić: </label>
+                <input type="date" value="2021-09-21" min="2021-01-01" max="2022-12-31" />
+                <button>Dodaj</button>
             </div>
         );
     }
